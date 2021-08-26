5e4e56ccc68222cfa229433bdc1a907394517cd8
Bruno Lopes <eusoubruno7@gmail.com>
2021-08-26 09:24:05 -0300

    feat: password hash

--- src/controllers/UseerController.js
+++ src/controllers/UseerController.js
@@ -1,10 +1,12 @@
+const { hash } = require('bcryptjs');
 const User = require('../models/User');
 
 module.exports = {
   async store(request, response) {
     const { name, email, password } = request.body;
+    const passwordHashed = await hash(password, 8);
     try {
-      const user = await User.create({ name, email, password });
+      const user = await User.create({ name, email, password: passwordHashed });
       return response.status(200).json(user);
     } catch (err) {
       return response.status(400).json({ error: err.message });
--- src/models/User.js
+++ src/models/User.js
@@ -14,6 +14,7 @@ const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     requiered: true,
+    unique: true,
   },
   password: {
     type: String,
