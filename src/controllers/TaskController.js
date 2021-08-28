5cd2bc47dc1d661e95f6104059c2a400d608de86
Bruno Lopes <eusoubruno7@gmail.com>
2021-08-27 23:35:51 -0300

    fix: adjusting in the list all tasks

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -17,24 +17,48 @@ module.exports = {
     const { user_id } = request;
 
     try {
-      const tasks = await Task.find({ user_id });
+      let tasks = await Task.find({ user_id });
 
-      const tasksMap = tasks.map(task => {
+      const tasksMap = tasks.map(async task => {
         const currentDate = new Date();
         let difference = task.deadline.getTime() - currentDate.getTime();
-        difference = difference / (60 * 60);
+        difference = difference / (1000 * 60 * 60);
         if (difference > 5) {
-          task.status = 'no urgency';
+          await Task.collection.updateOne(
+            { id: task.id },
+            {
+              $set: { status: 'no urgency' },
+              $currentDate: { updatedAt: true },
+            },
+          );
         } else if (difference > 3) {
-          task.status = 'close';
-        } else if (difference > 1 || difference < 1) {
-          task.status = 'ugercy';
+          await Task.collection.updateOne(
+            { id: task.id },
+            { $set: { status: 'close' }, $currentDate: { updatedAt: true } },
+          );
+        } else if (difference > 0 && difference < 1) {
+          await Task.collection.updateOne(
+            { id: task.id },
+            {
+              $set: { status: 'urgency' },
+              $currentDate: { updatedAt: true },
+            },
+          );
         } else if (difference <= 0) {
-          task.status = 'expires';
+          await Task.collection.updateOne(
+            { id: task.id },
+            {
+              $set: { status: 'expires' },
+              $currentDate: { updatedAt: true },
+            },
+          );
         }
         return task;
       });
-      return response.status(200).json(tasksMap);
-    } catch (err) {}
+      tasks = await Task.find({ user_id });
+      return response.status(200).json(tasks);
+    } catch (err) {
+      throw new Error(err.message);
+    }
   },
 };
