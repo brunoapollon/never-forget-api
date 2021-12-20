812aea74135b4f1bff7cceba8308248055399564
Bruno Lopes <eusoubruno7@gmail.com>
2021-12-20 12:50:41 -0300

    feat: adding cors

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require('dotenv/config');
+const cors = require('cors');
 
 const handleErros = require('./middlewares/handleErrors');
 
@@ -8,6 +9,7 @@ const routes = require('./index.routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(handleErros);
